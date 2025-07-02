036c20e56605ddf23a89d3942066d1d8ce4c7d4e
Paulo Alvarez <paulo.manuel.alvarez@gmail.com>
2025-07-02 20:54:37 -0300

    implementa CRUD en admin

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,4 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Vino } from '../lista-vinos/Vino';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { VinoDataService } from '../vino-data.service';
+
+interface VinoConId extends Vino {
+  id: string;
+}
 
 @Component({
   selector: 'app-admin',
@@ -6,6 +13,78 @@ import { Component } from '@angular/core';
   templateUrl: './admin.component.html',
   styleUrl: './admin.component.scss'
 })
-export class AdminComponent {
+export class AdminComponent implements OnInit {
+  vinos: VinoConId[] = [];
+  modoEdicion: boolean = false;
+  vinoEditandoId: string | null = null;
 
+  formVino = new FormGroup({
+    nombre: new FormControl('', [Validators.required]),
+    cepa: new FormControl('', [Validators.required]),
+    bodega: new FormControl('', [Validators.required]),
+    precio: new FormControl(0, [Validators.required, Validators.min(1)]),
+    stock: new FormControl(0, [Validators.required, Validators.min(0)]),
+    // coloco una imagen válida por defecto para cuando creo un vino 
+    // asi no tengo que buscar una nueva, solo para etapa de desarrollo
+    imagen: new FormControl('https://www.clarin.com/img/2024/06/21/W0lhpAYFU_720x0__1.jpg', 
+                            [Validators.required]),
+    liquidacion: new FormControl(false)
+  });
+
+  constructor(private vinoService: VinoDataService) { }
+
+  ngOnInit() {
+    this.cargarVinos();
+  }
+
+  cargarVinos() {
+    this.vinoService.obtenerVinos().subscribe(vs => this.vinos = vs as VinoConId[]);
+  }
+
+
+  onSubmit() {
+    if (this.formVino.invalid) return;
+
+    const vinoFormValue = this.formVino.value as Vino;
+
+    if (this.modoEdicion && this.vinoEditandoId) {
+      // Actualizo solo los campos que existen en MockAPI (sin cantidad)
+      this.vinoService.actualizarVino(this.vinoEditandoId, vinoFormValue).subscribe(() => {
+        this.resetFormulario();
+        this.cargarVinos();
+      });
+    } else {
+      // Creo con cantidad en 0 para uso del carrito
+      this.vinoService.crearVino({ ...vinoFormValue, cantidad: 0 }).subscribe(() => {
+        this.resetFormulario();
+        this.cargarVinos();
+      });
+    }
+  }
+
+  editarVino(vino: VinoConId) {
+    this.formVino.setValue({
+      nombre: vino.nombre,
+      cepa: vino.cepa,
+      bodega: vino.bodega,
+      precio: vino.precio,
+      stock: vino.stock,
+      imagen: vino.imagen,
+      liquidacion: vino.liquidacion
+    });
+    this.vinoEditandoId = vino.id;
+    this.modoEdicion = true;
+    // para que me lleve al comienzo del formulario cuando selecciono editar
+    window.scrollTo({ top: 100, behavior: 'smooth' });
+  }
+
+  eliminarVino(id: string) {
+    this.vinoService.eliminarVino(id).subscribe(() => this.cargarVinos());
+  }
+
+  resetFormulario() {
+    this.formVino.reset();
+    this.modoEdicion = false;
+    this.vinoEditandoId = null;
+  }
 }
--- src/app/vino-data.service.ts
+++ src/app/vino-data.service.ts
@@ -3,17 +3,29 @@ import { Injectable } from '@angular/core';
 import { Vino } from './lista-vinos/Vino';
 import { Observable } from 'rxjs';
 
-const URL= 'https://68500da9e7c42cfd17972d0f.mockapi.io/vinos'
+const URL = 'https://68500da9e7c42cfd17972d0f.mockapi.io/vinos'
 
 @Injectable({
   providedIn: 'root'
 })
 export class VinoDataService {
 
-    constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) { }
 
-    public obtenerVinos(): Observable<Vino[]> {
-      return this.http.get<Vino[]>(URL);
-    }
-    
+  public obtenerVinos(): Observable<Vino[]> {
+    return this.http.get<Vino[]>(URL);
   }
+
+  public crearVino(vino: Vino): Observable<Vino> {
+    return this.http.post<Vino>(URL, vino);
+  }
+
+  public actualizarVino(id: string, vino: Omit<Vino, 'cantidad'>): Observable<Vino> {
+    return this.http.put<Vino>(`${URL}/${id}`, vino);
+  }
+
+  public eliminarVino(id: string): Observable<void> {
+    return this.http.delete<void>(`${URL}/${id}`);
+  }
+
+}
